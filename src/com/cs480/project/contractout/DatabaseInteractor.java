b7229a845e1f51dc0ba882c48a415af9bbd5f7e8
Ben Clifford <ben.w.clifford@gmail.com>
2013-12-10 20:56:11 -0800

    Updated DatabaseInterceptor
    DatabaseInterceptor connects to a SQLite database now (this commit also
    includes a SQLite database with some data).

--- src/com/cs480/project/contractout/DatabaseInteractor.java
+++ src/com/cs480/project/contractout/DatabaseInteractor.java
@@ -1,6 +1,12 @@
 package com.cs480.project.contractout;
 
+import android.database.sqlite.*;
+
 public class DatabaseInteractor {
+   
+   // Globals
+   SQLiteDatabase db;
+	
    public static String logIn(String args){
       if(args.equals("user_name=<danialr@gmail.com>;password=<adzq>"))
          return "<123456>, <danialr@gmail.com>, <adzq>, <Danial Racker>, <1234 Fake St>, <Springfield>, <Illinois>, <12334>, <9515551234>";
@@ -8,8 +14,9 @@ public class DatabaseInteractor {
          return "False";
    }
    
-   public static String getData(String args){
-      return "filler";
+   public static String[][] getData(String args){
+	  String[][] filler = {{"filler"}};
+      return filler;
    }
    
    public static boolean insertData(String args){
@@ -21,7 +28,8 @@ public class DatabaseInteractor {
    }
 
    public static boolean initializer() {
-      // TODO Auto-generated method stub
+      String fileName = "user_data.db";
+      db = new SQLiteDatabase(fileName);
       return true;
    }
 }
