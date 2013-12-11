c1e6ed4fc0a82d05fcef8cb080bce5fcf0e11f19
Ben Clifford <ben.w.clifford@gmail.com>
2013-12-11 11:23:25 -0800

    Final test of SQLite connection.

--- src/com/cs480/project/contractout/DatabaseInteractor.java
+++ src/com/cs480/project/contractout/DatabaseInteractor.java
@@ -1,23 +1,38 @@
 package com.cs480.project.contractout;
 
 import android.app.Activity;
+import android.content.res.*;
+import android.content.*;
+import android.database.Cursor;
 import android.database.sqlite.*;
 
-public class DatabaseInteractor extends Activity{
+public class DatabaseInteractor extends Activity {
    
    // Globals
-   static SQLiteDatabase db;
+   static String PATH;
+   AssetManager assetMan;
+   AssetFileDescriptor file;
+   SQLiteDatabase db;
 	
    /*
     * Checks username and password against the database to ensure that there is a matching 
     * entry.
     */
    public static String logIn(String username, String password) {
-	  String whereClause = "username='" + username + "' and password='" + password + "'";
-	  curs = query ("Users", null, whereClause, [], null, null, null, null);
+	   String whereClause = "username='" + username + "' and password='" + password + "'";
+       Cursor curs = db.query ("Users", null, whereClause, null, null, null, null, null);
+	   
+//	  String fileName = "TSV_Users.txt";
+//	  AssetFileDescriptor file = assetMan.openFd(fileName); 
+//	  AutoCloseInputStream input = new AssetFileDescriptor.AutoCloseInputStream(file);
+//	  byte[] buffer = new byte[];
+//	  input.read(buffer);
+	  // Dump file into array (see Alex?)
+	  // Search array for matching username and password
+	  // return "False" or string in Alex's format with <>'s
    }
    
-   public static String[][] getData(String args){
+   public static String[][] getData(String args) {
       String[][] temp = new String[3][];
       if(args.equals("Jobs;creator=<123456>;end_date=")){
          temp[1] = ("1;123 Sesame St;Los Angeles;9001;12/1/2013;12/1/2013;My kitchen sink won't drain.;20;10;123456;305").split(";");
@@ -30,17 +45,22 @@ public class DatabaseInteractor extends Activity{
       return temp;
    }
    
-   public static int insertData(String args){
+   public static int insertData(String args) {
       return 0;
    }
    
-   public static int updateData(String args){
+   public static int updateData(String args) {
       return 0;
    }
 
    public static boolean initializer(String filepath) {
-      String fileName = filepath + "user_data.db";
-      db = SQLiteDatabase.openOrCreateDatabase(fileName, null);
+      PATH = filepath;
+      Context context = new Context();
+      assetMan = context.getAssets();
+      String fileName = "user_data.db";
+      file = assetMan.openFd(fileName); 
+      db = SQLiteDatabase.openOrCreateDatabase(file, null);
+      // @todo Check AssetManager.list() for all expected file names and creat all missing ones
       return true;
    }
 }
